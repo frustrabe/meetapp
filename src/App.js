69b8f153b6aa346aabeadc4aba0b31afbb0ba48d
frustrabe <lukas.forner@gmail.com>
2022-05-05 19:32:12 +0200

    created separation, added routing, nav bar and styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupsPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div> Ahoj Ivanka </div>
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
