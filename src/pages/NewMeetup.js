db8870b4eb382ba833c72b0ec0ba79dba9175d62
frustrabe <lukas.forner@gmail.com>
2022-05-06 13:01:25 +0200

    added form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,11 @@
 import React from "react";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
     return (
         <section>
             <h1>Add New Meetup</h1>
-
+            <NewMeetupForm />
         </section>
     );
 }
