f784d8074d2cfcd6bec9572b2c135ac4d208b7d1
frustrabe <lukas.forner@gmail.com>
2022-05-07 10:52:24 +0200

    setting up firebase, programatic navigation

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://meet-app-d9c69-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                header: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/', { replace: true });
+        });
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </section>
     );
 }
